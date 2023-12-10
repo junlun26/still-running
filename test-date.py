3a5f1ad0c2a6af081f1a095320fee7a73e33fd3e
junlun26 <142468461+junlun26@users.noreply.github.com>
2023-12-10 14:05:04 +0800

    test-date.py

--- test-date.py
+++ test-date.py
@@ -6,7 +6,7 @@ gc = pygsheets.authorize(service_account_file='./gsheet.json')
 sht = gc.open_by_url('https://docs.google.com/spreadsheets/d/1cxvkIggrRHYnZy0oUH-leCFHy3gWdJGEi7XC3i1Hv3c/edit#gid=0')
 wks_list = sht.worksheets()
 def job():
-    date = datetime.datetime.now().date()
+    date = str(datetime.datetime.now().date())
     wks_list.update_value('A1', date)
 schedule.every().minute.at(":00").do(job)
 
