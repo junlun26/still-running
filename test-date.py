5f48e4ce62f7dcf41e00791cd7ee1efc705692b1
junlun26 <142468461+junlun26@users.noreply.github.com>
2023-12-10 14:34:42 +0800

    test-date.py

--- test-date.py
+++ test-date.py
@@ -5,12 +5,12 @@ import pygsheets
 gc = pygsheets.authorize(service_account_file='./gsheet.json')
 sht = gc.open_by_url('https://docs.google.com/spreadsheets/d/1cxvkIggrRHYnZy0oUH-leCFHy3gWdJGEi7XC3i1Hv3c/edit#gid=0')
 wks_list = sht.sheet1
-def job():
-    date = str(datetime.datetime.now())
-    wks_list.update_value('A1', date)
-schedule.every().minute.at(":00").do(job)
+#def job():
+#    date = str(datetime.datetime.now())
+#    wks_list.update_value('A1', date)
+#schedule.every().minute.at(":00").do(job)
 
 #等待定時任務
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+#while True:
+#    schedule.run_pending()
+#    time.sleep(1)
